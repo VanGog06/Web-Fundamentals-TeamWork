0f91d24c21d13fc3930f1ea82d2bd01f3d94c501
Georgi Stankov <vangog06406@gmail.com>
2017-03-18 18:36:26 +0200

    Added slide-in effect

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     hamburgerMenu();
     smoothScroll();
     showHideTellersDescription();
+    scrollAnimation();
+    changeNakovsOpacity();
 });
 
 function smoothScroll() {
@@ -59,8 +61,42 @@ function hamburgerMenu() {
 }
 
 function showHideTellersDescription() {
-    $(".tellers section article").hover(function() {
+    $(".tellers section article").hover(function () {
         $(this).find('h2').toggle('slow');
         $(this).find('p').toggle('slow');
+        $(this).find('img').toggleClass('toggle-opacity');
     });
+}
+
+function changeNakovsOpacity() {
+    $('.mag-nakov').hover(function() {
+        $(this).find('img').toggleClass('toggle-opacity');
+    });
+}
+
+function scrollAnimation() {
+    let $animation_elements = $('.animation-element');
+    let $window = $(window);
+
+    function check_if_in_view() {
+        let window_height = $window.height();
+        let window_top_position = $window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
+
+        $.each($animation_elements, function () {
+            let $element = $(this);
+            let element_height = $element.outerHeight();
+            let element_top_position = $element.offset().top;
+            let element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+            }
+        });
+    }
+
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
 }
